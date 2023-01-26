ad7dc45046c7a8337eb9aa8a43799001088756b6
Rami <rami.khalil.charbel@gmail.com>
2023-01-26 18:28:19 +0100

    remove barbar binds

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -4,10 +4,3 @@ if not ok then
     return
 end
 buffer.setup({})
-
-local bind = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
-bind('n', '<leader>x', '<cmd>BufferClose<CR>', opts) -- quit 
-bind('n', 'L', '<cmd>BufferNext<CR>', opts)
-bind('n', 'H', '<cmd>BufferPrevious<CR>', opts)
